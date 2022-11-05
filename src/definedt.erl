a6ff64d1449b295946cf89938b65de28c19a059b
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-11-05 16:09:16 -0600

    update atom 'last_updated'

--- src/definedt.erl
+++ src/definedt.erl
@@ -7,4 +7,4 @@ dt_properties(dt_id)        -> "{\"dt_id\" : \"flower_peony_01\"}";
 dt_properties(e_tag)        -> "{\"e_tag\" : \"94271cba-f780-46a5-a646-0f0da8d21ecc\"}";
 dt_properties(flower_name)  -> "{\"flower_name\" : \"peony\"}";
 dt_properties(humidity)     -> F = 30 + rand:uniform(), String = io_lib:format("~.5f",[F]),"{\"humidity\" : \"" ++ String ++"\"}";  
-dt_properties(last_update)  -> "{\"last_update\" : \"2022-11-04T18:39:22.0353689Z\"}".
+dt_properties(last_updated)  -> "{\"last_updated\" : \"2022-11-04T18:39:22.0353689Z\"}".
